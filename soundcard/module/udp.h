3b2c36b4a0f11a6fbabc64caeaada055b56b8749
unknown <mysoulrage@gmail.com>
2015-03-11 14:48:00 +0300

    empty usb modules

---
+++ soundcard/module/udp.h
@@ -0,0 +1,16 @@
+/*
+ * udp.h
+ *
+ * Created: 11.03.2015 14:29:42
+ *  Author: ale
+ */ 
+
+
+#ifndef UDP_H_
+#define UDP_H_
+
+
+
+
+
+#endif /* UDP_H_ */
---
+++ soundcard/module/usb.c
@@ -0,0 +1,8 @@
+/*
+ * usb.c
+ *
+ * Created: 11.03.2015 14:28:35
+ *  Author: ale
+ */ 
+
+#include "usb.h"
