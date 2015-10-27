f7058910c000b032b5d45901277666643e4b411e
unknown <mysoulrage@gmail.com>
2015-10-27 13:25:31 +0300

    Demo version with usb-audio.

--- soundcard/module/codec/pcm3793.c
+++ soundcard/module/codec/pcm3793.c
@@ -16,12 +16,12 @@ void pcm3793_init() {
 	
 	/* Set analog volume for right Tel. channel
 		and disable analog mute. */
-	pcm3793_hpr_vol(PCM_R65_HRV(0x27));
+	pcm3793_hpr_vol(PCM_R65_HRV(0x33));
 	
 	/* SPL/R disable */
 	
 	/* DAC block */
-	pcm3793_dar(PCM_R69_ATR(0x27));
+	pcm3793_dar(PCM_R69_ATR(0x33));
 	pcm3793_dac_format(PCM_R70_PFM_LJust);
 	pcm3793_dac_over();
 	pcm3793_dac_filter_dem(PCM_R70_DEM_OFF);
@@ -44,8 +44,8 @@ void pcm3793_init() {
 	pcm3793_pmxr_on();
 	
 	/* Choose switchers. */
-	pcm3793_switch(PCM_R88_SW1 | PCM_R88_SW6);
-//	pcm3793_switch(PCM_R88_SW5 | PCM_R88_SW2);
+//	pcm3793_switch(PCM_R88_SW1 | PCM_R88_SW6);
+	pcm3793_switch(PCM_R88_SW5 | PCM_R88_SW2);
 	
 	/* Turn on HPR. */
 	void pcm3793_phpr_up();
@@ -112,11 +112,11 @@ void pcm3793_init() {
 	//__DEBUG(LOG_LVL_HIGH, "[audio]\tConfigured pcm3793");
 	///* wait 450 ms */
 	////TODO: remade this shit
-	//volatile unsigned long long i = 0;
-	//for(i = 0; i < 100000; i++) {
-		//__NOP;
-	//}
-	//pcm3793_write(PCM_DAC_SPA_HPA, 0xff);
+	volatile unsigned long long i = 0;
+	for(i = 0; i < 100000; i++) {
+		__NOP;
+	}
+	pcm3793_write(0x49, 0xff);
 	
 	__DEBUG(LOG_LVL_HIGH, "[audio]\tStarted in master mode");
 	
